437b33b3304cc632c9c880c62b7d8eca7fd2aa3a
lanyu7 <lanyu7@live.com>
2020-05-03 17:49:02 +0800

    v2.10
    修复常驻饰品不能互选

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ local merge = AQSELF.merge
 
 -- 配置 --
 
-AQSELF.version = "v2.9"
+AQSELF.version = "v2.10"
 
 -- AQSELF.enableDebug = true          -- 调试开关
 AQSELF.enableDebug = false          -- 调试开关
